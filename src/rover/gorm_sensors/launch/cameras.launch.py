1b9fc8d32a777c5ed76c7412df5410ed29bd5565
J-Thorhauge <jonas.thorhauge@gmail.com>
2025-10-02 11:41:06 +0200

    Added delay to the back camera launch

--- src/rover/gorm_sensors/launch/cameras.launch.py
+++ src/rover/gorm_sensors/launch/cameras.launch.py
@@ -1,6 +1,7 @@
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 import os
@@ -11,23 +12,29 @@ from launch.substitutions import LaunchConfiguration, Command
 def generate_launch_description():
     ld = LaunchDescription()
 
-    zed_tracking = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            FindPackageShare('gorm_sensors'), '/launch/zed_camera.launch.py'
-        ]),
-        launch_arguments={
-            'camera_model': 'zed2i',
-            'serial_number': '37915676',
-            'camera_id': '0',
-            'node_name': 'zed_tracking',
-            'grab_resolution': 'HD1080', # 'HD720',  # The native camera grab resolution. 'HD2K', 'HD1080', 'HD720', 'VGA', 'AUTO'
-            'gnss_fusion_enabled': 'false',  # Enable GNSS fusion
-            'namespace': 'zed_tracking',  # Namespace for the camera node
-            'initial_base_pose': '[0.28, 0.0, 0.225, 0.0, 0.0, 0.0]',  # Initial pose of the base frame with respect to the camera frame
-            'pos_tracking': 'true',  # Enable positional tracking
-            'publish_tf': 'true',  # Publish TF for the camera
-            'publish_map_tf': 'true',  # Publish map TF for the camera
-        }.items()
+    # Delayed zed_tracking launch
+    zed_tracking = TimerAction(
+        period=20.0,  # Delay in seconds
+        actions=[
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    FindPackageShare('gorm_sensors'), '/launch/zed_camera.launch.py'
+                ]),
+                launch_arguments={
+                    'camera_model': 'zed2i',
+                    'serial_number': '37915676',
+                    'camera_id': '0',
+                    'node_name': 'zed_tracking',
+                    'grab_resolution': 'HD1080',
+                    'gnss_fusion_enabled': 'false',
+                    'namespace': 'zed_tracking',
+                    'initial_base_pose': '[0.28, 0.0, 0.225, 0.0, 0.0, 0.0]',
+                    'pos_tracking': 'true',
+                    'publish_tf': 'true',
+                    'publish_map_tf': 'true',
+                }.items()
+            )
+        ]
     )
 
     zed_front = IncludeLaunchDescription(
@@ -39,20 +46,19 @@ def generate_launch_description():
             'serial_number': '35803121',
             'camera_id': '1',
             'node_name': 'zed_front',
-            'grab_resolution': 'HD1080', # 'VGA',  # The native camera grab resolution. 'HD2K', 'HD1080', 'HD720', 'VGA', 'AUTO'
-            'pos_tracking': 'false',  # Enable positional tracking
-            'publish_tf': 'true',  # Publish TF for the camera
-            'publish_map_tf': 'false',  # Publish map TF for the camera
-            'namespace': 'zed_front',  # Namespace for the camera node
+            'grab_resolution': 'HD1080',
+            'pos_tracking': 'false',
+            'publish_tf': 'true',
+            'publish_map_tf': 'false',
+            'namespace': 'zed_front',
         }.items()
     )
+
     static_tf_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_transform_publisher_zed_base',
         arguments=['-0.28', '0.0', '-0.225', '0.0', '0.0', '0.0', 'zed_camera_link', 'base_link'],
-        # Arguments: x y z yaw pitch roll parent_frame child_frame
-        # Note: yaw, pitch, roll are in radians!
     )
 
     camera_pose = Node(
@@ -64,4 +70,5 @@ def generate_launch_description():
     ld.add_action(static_tf_node)
     ld.add_action(zed_tracking)
     ld.add_action(zed_front)
+
     return ld
