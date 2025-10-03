84de6229f09903023b71ca339dcff3f30d451836
Skepingawich <93713249+Skepingawich@users.noreply.github.com>
2025-10-03 13:45:26 +0200

    removed sync note again and fixe error in parameters, should function now

--- src/rover/gorm_sensors/launch/cameras.launch.py
+++ src/rover/gorm_sensors/launch/cameras.launch.py
@@ -42,6 +42,7 @@ def generate_launch_description():
             'grab_resolution': 'HD1080', # 'VGA',  # The native camera grab resolution. 'HD2K', 'HD1080', 'HD720', 'VGA', 'AUTO'
             'pos_tracking': 'false',  # Enable positional tracking
             'publish_tf': 'true',  # Publish TF for the camera
+            'publish_imu_tf': 'true'
             'publish_map_tf': 'false',  # Publish map TF for the camera
             'namespace': 'zed_front',  # Namespace for the camera node
         }.items()
--- src/rover/gorm_vslam/launch/vslam.launch.py
+++ src/rover/gorm_vslam/launch/vslam.launch.py
@@ -37,7 +37,23 @@ def generate_launch_description():
     #     arguments=['0', '0', '0', '0', '0', '0', 'zed_front_base_link', 'zed_front_left_camera_optical_frame'],#x value differs from the report, but was experimentet to match better with this value.
     #     output='screen'  # Ensure logs are visible
     # )
-
+    # sync_node = Node(
+    #         package='rtabmap_sync',
+    #         executable='rgbd_sync',
+    #         name='rgbd_sync',
+    #         output='screen',
+    #         parameters=[{
+    #             'compressed': 'true',
+    #             'approx_sync': True,   # Set to False if using exact sync
+    #             'queue_size': 10
+    #         }],
+    #         remappings=[
+    #             ('rgb/image', '/zed_front/zed/rgb_gray/image_rect_gray/compressed'),
+    #             ('depth/image', '/zed_front/zed/depth/depth_registered/compressedDepth'),
+    #             ('rgb/camera_info', '/zed_front/zed/rgb_gray/camera_info'),
+    #             ('rgbd_image', 'rtabmap/rgbd_image')
+    #         ]
+    #     )
   
     rtabmap_launch_dir = FindPackageShare('rtabmap_launch').find('rtabmap_launch')
 
@@ -46,14 +62,22 @@ def generate_launch_description():
             os.path.join(rtabmap_launch_dir, 'launch', 'rtabmap.launch.py')
         ),
         launch_arguments={
+            'compressed': 'true',
+            'rtabmap_args': "--delete_db_on_start ",
             'rgb_topic': '/zed_front/zed/rgb_gray/image_rect_gray',
             'depth_topic': '/zed_front/zed/depth/depth_registered',
             'camera_info_topic': '/zed_front/zed/rgb_gray/camera_info',
-            'depth_camera_info_topic': '/zed_front/zed/depth/camera_info',
-            'imu_topic': '/zed_front/zed/imu/data',
+            #'subscribe_rgbd': 'true',    # to be used if using the sync node 
             'frame_id': 'zed_camera_link',
             'approx_sync': 'true',
-            'wait_imu_to_init': 'true'
+            'use_sim_time': 'true',
+            #'wait_imu_to_init': 'true',
+            #'imu_topic' :'/zed/zed_node/imu/data',
+            'qos': '1',
+            'topic_queue_size': '100',
+            'sync_queue_size': '300',
+            'rviz':'true', 
+            'rtabmap_viz': 'true'
         }.items()
     )
 
@@ -72,7 +96,7 @@ def generate_launch_description():
         DeclareLaunchArgument('rtabmap_viz',  default_value='false',  description='Launch RTAB-Map UI (optional).'),
         DeclareLaunchArgument('rviz',         default_value='false',   description='Launch RVIZ (optional).'),
         DeclareLaunchArgument('localization', default_value='false',  description='Launch in localization mode.'),
-        
+        #sync_node,
         rtabmap_launch,
         # static_transform_publisher_fcam,
         # static_transform_publisher_fimu,
