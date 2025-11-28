4646477cb6b32bf4b34c7ac10a99d0b090bc751a
Skepingawich <93713249+Skepingawich@users.noreply.github.com>
2025-11-28 10:14:50 +0100

    Feat: changed map_republisher to cace map and republish last cached map if rtab map fails.

--- src/rover/gorm_vslam/scripts/map_republisher.py
+++ src/rover/gorm_vslam/scripts/map_republisher.py
@@ -3,22 +3,42 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
 
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import OccupancyGrid
+
 class MapRepublisher(Node):
     def __init__(self):
         super().__init__('map_republisher')
+
+        self.last_map = None
+
         self.subscription = self.create_subscription(
             OccupancyGrid,
             'rtabmap/map',
             self.map_callback,
             10)
+
         self.publisher = self.create_publisher(
             OccupancyGrid,
             '/map',
             10)
-        self.get_logger().info('Map republisher started: rtabmap/map → /map')
+
+        # Timer to periodically republish the last map:
+        self.timer = self.create_timer(
+            0.5,             # publish at 2 Hz 
+            self.timer_callback)
+
+        self.get_logger().info('Map republisher started')
 
     def map_callback(self, msg):
-        self.publisher.publish(msg)
+        self.last_map = msg
+
+    def timer_callback(self):
+        if self.last_map is not None:
+            # Always publish latest known map
+            self.publisher.publish(self.last_map)
 
 def main(args=None):
     rclpy.init(args=args)
